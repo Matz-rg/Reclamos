e0ae5fdcd104694325a51aecdffd9ce4824224ac
Matias Fregapane <matiasfregapane50@gmail.com>
2025-10-31 10:57:07 -0300

    Configuracion de seguridad (User)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,14 +2,14 @@
 
 namespace App\Entity;
 
-use App\Repository\UsuarioAdminRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-#[ORM\Entity(repositoryClass: UsuarioAdminRepository::class)]
+#[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-class UsuarioAdmin implements UserInterface, PasswordAuthenticatedUserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\UsuarioAdmin;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
@@ -10,13 +10,13 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
- * @extends ServiceEntityRepository<UsuarioAdmin>
+ * @extends ServiceEntityRepository<User>
  */
-class UsuarioAdminRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, UsuarioAdmin::class);
+        parent::__construct($registry, User::class);
     }
 
     /**
@@ -24,7 +24,7 @@ class UsuarioAdminRepository extends ServiceEntityRepository implements Password
      */
     public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
     {
-        if (!$user instanceof UsuarioAdmin) {
+        if (!$user instanceof User) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
         }
 
@@ -34,7 +34,7 @@ class UsuarioAdminRepository extends ServiceEntityRepository implements Password
     }
 
 //    /**
-//     * @return UsuarioAdmin[] Returns an array of UsuarioAdmin objects
+//     * @return User[] Returns an array of User objects
 //     */
 //    public function findByExampleField($value): array
 //    {
@@ -48,7 +48,7 @@ class UsuarioAdminRepository extends ServiceEntityRepository implements Password
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?UsuarioAdmin
+//    public function findOneBySomeField($value): ?User
 //    {
 //        return $this->createQueryBuilder('u')
 //            ->andWhere('u.exampleField = :val')
